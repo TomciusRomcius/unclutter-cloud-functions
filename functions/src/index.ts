83217c393c7e49b63b2f6b6416f332c8b0888ca5
Tomas <tomas.ew88@gmail.com>
2024-01-11 22:51:37 +0200

    Encryption work

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,37 +7,85 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const AWS = require("aws-sdk");
-const dotenv = require('dotenv');
-dotenv.config({path: ".env"});
-
 const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const AWS_KMS_KEY_ID = process.env.AWS_KMS_KEY_ID;
 
 AWS.config.update({
-  region: "eu-west-1",
+  region: "eu-north-1",
   accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
 });
 
 const kms = new AWS.KMS();
 
-exports.AWSencrypt =
-functions.region("europe-west1")
-.https.onCall(
-  (data: any, context: any) => 
-{
-  return kms.encrypt({
-    KeyId: AWS_KMS_KEY_ID,
-    Plaintext: data.plaintext
-  }).then((data: any) => {
-    return data.CiphertextBlob.toString('base64');
-  })
-  .catch((err: any) => {
-    console.log(err);
-    return "err";
+exports.AWSencrypt = functions
+  .region("europe-west1")
+  .https.onCall(async (data: any, context: any) => {    
+    const params = {
+      KeyId: AWS_KMS_KEY_ID,
+      Plaintext: Buffer.from(data.data),
+    };
+    try {
+      const result = await new Promise((resolve, reject) => {
+        kms.encrypt(params, (err: any, result: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result.CiphertextBlob.toString("base64"));
+          }
+        });
+      });
+
+      return { result: result, error: false };
+    } catch (error) {
+      return { result: "", error: true };
+    }
+    
   });
-});
+
+exports.AWSdecrypt = functions
+  .region("europe-west1")
+  .https.onCall(async (data: any, context: any) => {
+    const params = {
+      CiphertextBlob: Buffer.from(data.data, "base64"),
+    };
+    try {
+      const result = await new Promise((resolve, reject) => {
+        kms.encrypt(params, (err: any, result: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result.CiphertextBlob.toString("base64"));
+          }
+        });
+      });
+
+      return { result: result, error: false };
+    } catch (error) {
+      return { result: "", error: true };
+    }
+  });
+
+      /*
+    const params = {
+      CiphertextBlob: Buffer.from(data.data, "base64"),
+    };
+    try {
+      const result = await new Promise((resolve, reject) => {
+        kms.decrypt(params, (err: any, result: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result.Plaintext.toString("ascii"));
+          }
+        });
+      });
+
+      return { result: result, error: false };
+    } catch (error) {
+      return { result: "", error: true };
+    }
+    */
